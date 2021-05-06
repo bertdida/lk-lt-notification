6112725536830860097eb6dc3adc9d1b98fe0721
bertdida <38489033+bertdida@users.noreply.github.com>
2021-05-06 11:55:03 +0800

    Initial commit

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,14 +2,18 @@
 
 namespace LTN\Models;
 
+use LTN\Controllers\ConfigController;
 use LTN\Controllers\UserController;
 use mysqli;
 
 class User
 {
+    public $dbConn;
+
     public function __construct(mysqli $dbConn, int $id)
     {
-        $user = UserController::getById($dbConn, $id);
+        $this->dbConn = $dbConn;
+        $user = UserController::getById($this->dbConn, $id);
 
         if (!is_array($user)) {
             return null;
@@ -18,5 +22,10 @@ class User
         foreach ($user as $key => $value) {
             $this->$key = $value;
         }
+    }
+
+    public function getConfigs(): array
+    {
+        return ConfigController::getAll($this);
     }
 }
