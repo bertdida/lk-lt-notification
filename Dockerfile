e03c0c8b3ea3decdebddb9cb369840b2e7e3a55c
bertdida <38489033+bertdida@users.noreply.github.com>
2021-05-14 18:30:30 +0800

    Dockerize app - remove vlucas/phpdotenv from dependencies - add slim as dependency - setup docker

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM php:7.4-apache
+
+COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
+
+WORKDIR /home/appuser
+COPY . .
--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 define('ROOT_DIR', __DIR__);
-
 require_once ROOT_DIR . '/vendor/autoload.php';
 
-$dotenv = \Dotenv\Dotenv::createImmutable(ROOT_DIR);
-$dotenv->load();
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$customErrorHandler = function (Request $request, Throwable $exception) use ($app) {
+    $response = $app->getResponseFactory()->createResponse();
+    return $response
+        ->withJson(['error' => $exception->getMessage()])
+        ->withStatus($exception->getCode());
+};
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setDefaultErrorHandler($customErrorHandler);
+
+$app->get('/ping', function (Request $request, Response $response) {
+    $response->getBody()->write('pong');
+    return $response;
+});
+
+$app->run();
